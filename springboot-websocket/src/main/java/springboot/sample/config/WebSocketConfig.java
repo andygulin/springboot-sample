040832c8ca9ce7b043e402065b651c8973acacdd
andygulin <andygulin@hotmail.com>
2017-03-21 10:14:00 +0800

    代码优化...

--- springboot-websocket/src/main/java/springboot/sample/config/WebSocketConfig.java
+++ springboot-websocket/src/main/java/springboot/sample/config/WebSocketConfig.java
@@ -10,15 +10,16 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/websocket").withSockJS();
-	}
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket").withSockJS();
+    }
 
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.enableSimpleBroker("/topic");
-		registry.setApplicationDestinationPrefixes("/app");
-	}
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
 
 }
--- springboot-websocket/src/main/java/springboot/sample/controller/MessageController.java
+++ springboot-websocket/src/main/java/springboot/sample/controller/MessageController.java
@@ -1,15 +1,25 @@
 package springboot.sample.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import springboot.sample.bean.MessageRequest;
+import springboot.sample.bean.MessageResponse;
 
 @Controller
 public class MessageController {
 
-	@MessageMapping("/message")
-	@SendTo("/topic/room")
-	public String message(String message) throws Exception {
-		return message;
-	}
+    private static final String TOPIC_PREFIX = "/topic/room/";
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/message")
+    public void message(MessageRequest request) throws Exception {
+        MessageResponse response = new MessageResponse();
+        BeanUtils.copyProperties(request, response);
+        simpMessagingTemplate.convertAndSend(TOPIC_PREFIX + request.getRoomId(), response);
+    }
 }
